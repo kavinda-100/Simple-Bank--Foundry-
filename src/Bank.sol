7daee2790465834d03ab480d669e295729520e55
kavinda-100 <kavindarathnayake100@gmail.com>
2025-07-15 18:36:16 +0530

    Remove Counter contract and its associated test; add Bank and BankAccount contracts

---
+++ src/Bank.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+/**
+ * @title Bank
+ * @dev A simple bank contract that allows users to deposit ,withdraw, transfer, borrow funds.
+ * @author kavinda rathnayake
+ * @notice This contract is for educational purposes only. 
+ */
+contract Bank {
+    constructor() {
+        
+    }
+}
---
+++ src/BankAccount.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+/**
+ * @title BankAccount
+ * @dev A simple contract that represents a bank account with the ability to deposit and withdraw funds.
+ * @author kavinda rathnayake
+ * @notice This contract is for educational purposes only.
+ */
+contract BankAccount {
+    constructor() {
+        
+    }
+}
